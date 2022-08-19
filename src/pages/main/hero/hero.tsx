4d0c88bb77371c8496ca5f139267dc5d87c92294
ArturdaSilvaRezende <artursrezende@gmail.com>
2022-08-19 15:31:38 -0300

    Project structuring completed

---
+++ src/pages/main/hero/hero.tsx
@@ -0,0 +1,9 @@
+import * as C from "./style";
+
+export const Hero = () => {
+  return (
+    <C.HeroSection>
+      <h1>Hero</h1>
+    </C.HeroSection>
+  );
+};
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header/header";
+import { Hero } from "./hero/hero";
+
+export const Main = () => {
+  return (
+    <main>
+      <Header />
+      <Hero />
+    </main>
+  );
+};
