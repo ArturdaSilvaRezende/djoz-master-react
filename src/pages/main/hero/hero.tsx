4099dea4c75510299d4a9a48e6e84cde9ad23852
ArturdaSilvaRezende <artursrezende@gmail.com>
2022-08-23 18:55:15 -0300

    c-hero-main finished is responsive

--- src/pages/main/hero/hero.tsx
+++ src/pages/main/hero/hero.tsx
@@ -1,9 +1,25 @@
 import * as C from "./style";
+import { GrPlayFill } from "react-icons/gr";
 
 export const Hero = () => {
   return (
     <C.HeroSection>
-      <h1> .</h1>
+      <div className="heroSection__description">
+        <p>New Single</p>
+        <p>
+          <span>Feel the heart</span>
+          <span>beats</span>
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className="heroSection__btn">
+          <button type="button">
+            <GrPlayFill />
+          </button>
+        </div>
+      </div>
     </C.HeroSection>
   );
 };
